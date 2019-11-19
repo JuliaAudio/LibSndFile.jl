5ee4aae996b824c4a1157bbc22d6bcc6f06826e9
Spencer Russell <spencer.f.russell@gmail.com>
2019-11-18 23:11:28 -0500

    [WIP] Minimal modernization to get things building.
    This currently needs a branch of SampledSignals that fixes the FFTW and 
    DSP versions.

--- deps/build.jl
+++ deps/build.jl
@@ -1,9 +1,11 @@
+using Pkg
+Pkg.add("BinDeps")
+
 using BinDeps
-using Compat.Sys: BINDIR, isapple, iswindows
 
 @BinDeps.setup
 
-ENV["JULIA_ROOT"] = abspath(BINDIR, "../../")
+ENV["JULIA_ROOT"] = abspath(Sys.BINDIR, "../../")
 
 # include alias for WinRPM library
 libsndfile = library_dependency("libsndfile", aliases=["libsndfile-1"])
@@ -11,12 +13,12 @@ libsndfile = library_dependency("libsndfile", aliases=["libsndfile-1"])
 provides(AptGet, "libsndfile1-dev", libsndfile)
 provides(Pacman, "libsndfile", libsndfile)
 
-@static if isapple()
+@static if Sys.isapple()
     using Homebrew
     provides(Homebrew.HB, "libsndfile", libsndfile)
 end
 
-@static if iswindows()
+@static if Sys.iswindows()
     using WinRPM
     provides(WinRPM.RPM, "libsndfile1", libsndfile, os = :Windows)
 end
